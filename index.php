9d6d5fda30a08220be4dd07d378a73c3e1ed1f10
picklerules <136508588+picklerules@users.noreply.github.com>
2023-11-02 15:24:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -21,35 +21,44 @@ if ($url == '/') {
     $requestURL = ucfirst($requestURL);
     $controllerPath = __DIR__."/controller/Controller".$requestURL.".php";
 
-    if (file_exists($controllerPath)) {
-        require_once($controllerPath);
-        $controllerName = 'Controller'.$requestURL;
-        $controller = new $controllerName;
-        if (isset($url[1])) {
-            $method = $url[1];
-            if (method_exists($controller, $method)) {
-                if (isset($url[2])) {
-                    echo $controller->$method($url[2]);
+    if ($url == '/') {
+        require_once('controller/ControllerHome.php');
+        $controller = new ControllerHome;
+        echo $controller->index();
+    } else {
+        // controller
+        $requestURL = $url[0];
+        $requestURL = ucfirst($requestURL);
+        $controllerPath = __DIR__."/controller/Controller".$requestURL.".php";
+    
+        if (file_exists($controllerPath)) {
+            require_once($controllerPath);
+            $controllerName = 'Controller'.$requestURL;
+            $controller = new $controllerName;
+            if (isset($url[1])) {
+                $method = $url[1];
+                if (method_exists($controller, $method)) {
+                    if (isset($url[2])) {
+                        echo $controller->$method($url[2]);
+                    } else {
+                        echo $controller->$method();
+                    }
                 } else {
-                    // Redirection vers la page d'erreur 404 si l'id est manquant
+                    // Méthode non trouvée dans le contrôleur, rediriger vers une page d'erreur 404
                     header("HTTP/1.0 404 Not Found");
                     Twig::render('error404.php', ['title' => 'Error 404']);
                     exit;
                 }
             } else {
-                // Méthode non trouvée dans le contrôleur, rediriger vers une page d'erreur 404
-                header("HTTP/1.0 404 Not Found");
-                Twig::render('error404.php', ['title' => 'Error 404']);
-                exit;
+                echo $controller->index();
             }
         } else {
-            echo $controller->index();
+            // Redirection vers une page d'erreur 404
+            header("HTTP/1.0 404 Not Found");
+            Twig::render('error404.php', ['title' => 'Error 404']);
+            exit;
         }
-    } else {
-        // Redirection vers la page d'erreur 404 si le contrôleur est introuvable
-        header("HTTP/1.0 404 Not Found");
-        Twig::render('error404.php', ['title' => 'Error 404']);
-        exit;
     }
+    
 }
 ?>
